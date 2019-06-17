87ead6ba760d65690ac3fd0a6222f662b12095b4
wuufone <wuufone@gmail.com>
2019-06-17 16:28:43 +0800

    Initial Commit

---
+++ LearningSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearningSwift
+//
+//  Created by wuufone on 2019/6/17.
+//  Copyright © 2019 江武峯. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LearningSwiftTests/LearningSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  LearningSwiftTests.swift
+//  LearningSwiftTests
+//
+//  Created by wuufone on 2019/6/17.
+//  Copyright © 2019 江武峯. All rights reserved.
+//
+
+import XCTest
+@testable import LearningSwift
+
+class LearningSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
