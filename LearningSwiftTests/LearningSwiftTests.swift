342b410eacc4b25d6d5ae122fde785673a52c13b
wuufone <wuufone@gmail.com>
2019-06-17 17:20:52 +0800

    用 Unit Test 證明：
    16 等於 0b10000, 0o20, 0x10, 0x1p4。

--- LearningSwiftTests/LearningSwiftTests.swift
+++ LearningSwiftTests/LearningSwiftTests.swift
@@ -17,5 +17,9 @@ class LearningSwiftTests: XCTestCase {
     func testIntExpression() {
         XCTAssertEqual(10000000, 1000_0000)
         XCTAssertEqual(10000000, 1.0e7)
+        XCTAssertEqual(16, 0b10000)
+        XCTAssertEqual(16, 0o20)
+        XCTAssertEqual(16, 0x10)
+        XCTAssertEqual(16, 0x1p4)
     }
 }
